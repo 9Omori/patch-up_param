e713d6ad5cce1110a40c92ff3403a124a74c65ed
basil <118671833+9Omori@users.noreply.github.com>
2024-01-24 23:16:01 +0000

    Fix uninstall.sh

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -1,4 +1,3 @@
 #!/sbin/sh
 
-[ "$PWD" = "$MODPATH" ] || cd $MODPATH
-[ -f "./original-up_param.tar" ] && dd if=./original-up_param.tar of=/dev/block/by-name/up_param
+dd if=/data/adb/samsung_up_param_patcher/original-up_param.tar of=/dev/block/by-name/up_param
