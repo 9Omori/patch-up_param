5f60404fd93dbd8bb5adfd6c588e67fa25883f45
basil <118671833+9Omori@users.noreply.github.com>
2024-01-25 20:05:37 +0000

    Update uninstall.sh

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -1,3 +1,4 @@
 #!/sbin/sh
 
-dd if=/data/adb/modules/samsung_up_param_patcher/original-up_param.tar of=/dev/block/by-name/up_param
+cd /data/adb/modules/samsung_up_param_patcher
+dd if=original-up_param.tar of=/dev/block/by-name/up_param
