d5d6a87ecec7c6c7a7a8910f280b4b7eb8617aea
basil <118671833+9Omori@users.noreply.github.com>
2024-01-25 20:03:24 +0000

    Update customize.sh

--- module/customize.sh
+++ module/customize.sh
@@ -9,15 +9,13 @@ ui_print "- Extracting up_param"
     [ -d "./up_param" ] && rm -rf "./up_param"
     mkdir ./up_param
     dd if=/dev/block/by-name/up_param | tar -x -C ./up_param || abort "- Failed to extract up_param!"
+    ui_print "- Creating backup of original up_param"
+    dd if=/dev/block/by-name/up_param of=$MODPATH/original-up_param.tar
 }
 
 ui_print "- Patching up_param"
 {
     cd $MODPATH/up_param
-    if ! [ -f "../original-up_param.tar" ]; then
-        tar cf original-up_param.tar *
-        mv original-up_param.tar ..
-    fi
     for x in "svb_orange.jpg" "booting_warning.jpg"; do [ -f "${x}" ] && rm -f "${x}"; done
     rm -f *.tar
 }
