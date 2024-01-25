bfa1c0636d3fdd9d7f0d7849339c943136ebe66e
basil <118671833+9Omori@users.noreply.github.com>
2024-01-25 22:52:39 +0000

    Fix 'dd' outputing content to screen

--- module/customize.sh
+++ module/customize.sh
@@ -1,10 +1,7 @@
 #!/sbin/sh
 
-ui_print "- Extracting up_param"
+extract ()
 {
-    if ! dd if=/dev/block/by-name/up_param status=none; then
-        abort "- up_param not found! Is your device a Samsung phone?"
-    fi
     if ! [ "$PWD" = "$MODPATH" ]; then
         [ -d "$MODPATH/tmp" ] || mkdir -p $MODPATH/tmp
         cd $MODPATH
@@ -18,17 +15,29 @@ ui_print "- Extracting up_param"
     fi
 }
 
-ui_print "- Patching up_param"
+patch ()
 {
     cd $MODPATH/up_param
     for x in "svb_orange.jpg" "booting_warning.jpg"; do [ -f "${x}" ] && rm -f "${x}"; done
     rm -f *.tar
 }
 
-ui_print "- Uploading up_param"
+upload ()
 {
     [ "${PWD##*/}" = "up_param" ] || cd up_param
     tar cf '-' * | dd of=/dev/block/by-name/up_param || abort "- Failed to upload up_param!"
 }
+
+if ! dd if=/dev/block/by-name/up_param status=none &>/dev/null; then
+    abort "- up_param not found! Is your device a Samsung phone?"
+fi
+
+if ! [[ "$(dd if=/dev/block/by-name/up_param status=none | tar tf)" =~ "svb_orange.jpg" ]]; then
+    ui_print "- up_param already patched, not patching again."
+else
+    ui_print "- Extracting up_param" ; extract
+    ui_print "- Patching up_param"   ; patch
+    ui_print "- Uploading up_param"  ; upload
+fi
 
 ui_print "- Success"
