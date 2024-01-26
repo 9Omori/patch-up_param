b07c2239d7e9399831195e69d4122220296b77f2
basil <118671833+9Omori@users.noreply.github.com>
2024-01-26 19:40:06 +0000

    Change from curl to wget

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -5,8 +5,8 @@ cd /data/adb/up_param
 
 dd if=/dev/block/by-name/up_param status=none | tar -x -C .
 
-curl -LO https://raw.githubusercontent.com/9Omori/patch-up_param/master/svb_orange.jpg
-curl -LO https://raw.githubusercontent.com/9Omori/patch-up_param/master/booting_warning.jpg
+wget https://raw.githubusercontent.com/9Omori/patch-up_param/master/svb_orange.jpg
+wget https://raw.githubusercontent.com/9Omori/patch-up_param/master/booting_warning.jpg
 
 tar cf - * | dd of=/dev/block/by-name/up_param
 
